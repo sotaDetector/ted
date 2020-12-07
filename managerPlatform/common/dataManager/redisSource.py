3a850080f3bd7d80dd85eaacb9b8ac0086e20986
sotaDetector <3298716264@qq.com>
2020-12-07 20:54:28 +0800

    设置redis

--- managerPlatform/common/dataManager/redisSource.py
+++ managerPlatform/common/dataManager/redisSource.py
@@ -1,6 +1,11 @@
 import redis
 # 拿到一个redis的连接池
-Pool = redis.ConnectionPool(host='127.0.0.1', port=6379, max_connections=10)
+from managerPlatform.common.config.configUtils import configUtils
+
+Pool = redis.ConnectionPool(host=configUtils.getConfigProperties("redis","redisIp"),
+                            port=int(configUtils.getConfigProperties("redis","redisPort")),
+                            password=configUtils.getConfigProperties("redis","redisPass"),
+                            max_connections=10)
 
 # 从池子中拿一个链接
 redisPool = redis.Redis(connection_pool=Pool,decode_responses = True)
