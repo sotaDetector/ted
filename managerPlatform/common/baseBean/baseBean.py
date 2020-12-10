b9641707d5d08bca6dbcc026c83c63b39760ab5b
djw <3298716264@qq.com>
2020-12-10 16:05:49 +0800

    code

--- managerPlatform/common/baseBean/baseBean.py
+++ managerPlatform/common/baseBean/baseBean.py
@@ -1,16 +1,14 @@
 from datetime import datetime
-
-from requests import session
-
 from managerPlatform.common.commonUtils.ConstantUtils import ConstantUtils
 from managerPlatform.common.dataManager.mongoSource import mongoSource
+from flask import session
 
 
 class baseBean(mongoSource.mdb.Document):
     create_date = mongoSource.mdb.DateTimeField(default=datetime.now)
     update_date = mongoSource.mdb.DateTimeField(default=datetime.now)
     state=mongoSource.mdb.IntField(default=ConstantUtils.DATA_STATUS_ACTIVE)
-    userId=session['']
+    userId=mongoSource.mdb.LongField(default=session['userId'])
 
     # meta = {'allow_inheritance': True}
     meta = {
