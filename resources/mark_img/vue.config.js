3ff058d8aa18cb3b0338889ecc526530636fee69
Stacey0121 <Stacey0121@163.com>
2020-12-11 09:33:33 +0800

    push

--- resources/mark_img/vue.config.js
+++ resources/mark_img/vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
   },
   publicPath: './', //输出的根路径  默认是/ 如果你的网站是app.com/vue 这更改此配置项
-  outputDir: 'ted',
+  // outputDir: 'ted',
   assetsDir: 'static',
   //   devServer: {
   //     proxy: {
