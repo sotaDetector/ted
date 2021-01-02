00b5deacb26ddec1ba13d244f75ab8275ea0d8a6
djw <3298716264@qq.com>
2021-01-03 00:50:02 +0800

    解决文件名自增的问题

--- trainModelThread.py
+++ trainModelThread.py
@@ -482,7 +482,7 @@ class trainModelThread(threading.Thread):
             assert len(opt.cfg) or len(opt.weights), 'either --cfg or --weights must be specified'
             opt.img_size.extend([opt.img_size[-1]] * (2 - len(opt.img_size)))  # extend to 2 sizes (train, test)
             opt.name = 'evolve' if opt.evolve else opt.name
-            opt.save_dir = increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok | opt.evolve)  # increment run
+            opt.save_dir = Path(opt.project) / opt.name
 
         # DDP mode
         device = select_device(opt.device, batch_size=opt.batch_size)
